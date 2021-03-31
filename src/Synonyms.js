8f268075776bc0245490072f3b1d683dca542f49
Domi <dominika@nozbe.com>
2021-03-31 20:27:08 +0200

    Add synonyms styling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,15 +1,16 @@
 import React from "react";
+import "./styles/Synonyms.css";
+
 
 export default function Synonyms(props) {
     if (props.synonyms) {
         return (
-        <div className="synonyms">
-            <ul>
+        <div className="synonyms d-flex">
+            <p className="align-self-center">Synonyms:</p>
+            <ul className="d-flex flex-wrap justify-content-start mb-0">
                 {props.synonyms.map(function(synonym, index) {
                     return (
-                        <li key={index}>
-                        <span>{synonym}</span>
-                    </li>
+                        <li key={index} className="p-2 m-2">{synonym}</li>
                     )
                 })}
             </ul>
