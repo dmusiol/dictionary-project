807fa80bfa38f75ac7718a63d6816a7ef02573c5
Domi <dominika@nozbe.com>
2021-04-07 12:47:30 +0200

    Update footer and images layout

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles/Footer.css";
-import {AiOutlineGithub, AiFillFacebook, AiFillLinkedin } from 'react-icons/ai';
+import {AiOutlineGithub, AiFillFacebook, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
 
 
 
@@ -9,13 +9,14 @@ export default function Footer(){
         <div className="container footer-wraper">
             <div className="row footer-row">
                 <div className="col-md-6 d-flex flex-column p-0">
-                    <p>This project has been coded by Dominika Musioł.</p>
-                    <p><a href="https://github.com/dmusiol/dictionary-project">Open-sourced</a> on GitHub</p>
+                    <p className="pt-3">This project has been coded by Dominika Musioł.</p>
+                    <p className="source-link"><a href="https://github.com/dmusiol/dictionary-project">Open-sourced</a> on GitHub.</p>
                 </div>
                 <div className="col-md-6 media-icons">
                     <a href="https://www.facebook.com/dominikabm/" title="Facebook Profile" target="_blank" rel="noreferrer"><AiFillFacebook/></a>
                     <a href="https://www.linkedin.com/in/dominika-musioł/" title="LinkedIn Profile" target="_blank" rel="noreferrer"><AiFillLinkedin/></a>
                     <a href="https://github.com/dmusiol" title="GitHub Profile" target="_blank" rel="noreferrer"><AiOutlineGithub/></a>
+                    <a href="mailto:dominikamusiol07@gmail.com" title="Email contact"><AiOutlineMail /></a>
                 </div>
             </div>
         </div>
--- src/Meaning.js
+++ src/Meaning.js
@@ -3,6 +3,7 @@ import Synonyms from "./Synonyms";
 import "./styles/Meaning.css";
 
 export default function Meaning(props) {
+
     return (
         <div className="meanings-wraper">
             <h5 className="word-type">{props.meaning.partOfSpeech}</h5>
@@ -11,8 +12,8 @@ export default function Meaning(props) {
                     <div className="word-meaning" key={index}>
                         <div className="meaning-info">
                             <p className="definition">{definition.definition}</p>
-                            <p className="example">"{definition.example}"</p>
-                        </div>
+                            <p className="example">{definition.example}</p>
+                        </div> 
                         <div className="synonyms-list">
                             <Synonyms synonyms={definition.synonyms}/>
                         </div>
